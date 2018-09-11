96885350f4e12e1079c0e454b8419753259144e1
Dennis Kievits <denkievits@gmail.com>
2018-09-12 01:22:35 +0200

    Boilerplate changed for our use and structured frontend code

---
+++ frontend/src/components/containers/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div className="App">
+        <p className="App-title">Project 5 Webshop</p>
+      </div>
+    );
+  }
+}
+
+export default App;
