6847386e8e01d665842019ab21db81f1b4f5cbdd
Dennis Kievits <denkievits@gmail.com>
2018-09-12 18:03:59 +0200

    Icon update and home screen text

--- frontend/src/components/views/Home/Home.tsx
+++ frontend/src/components/views/Home/Home.tsx
@@ -7,6 +7,7 @@ const Home: React.SFC<IProps> = (props: IProps) => {
   return (
     <div className="Home">
       <p className="Home-Title">Project 5: Webshop</p>
+      <p className="Home-Subtitle">Marshmallow-Style</p>
     </div>
   );
 }
