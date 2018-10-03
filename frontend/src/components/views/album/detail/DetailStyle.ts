6e417c8b29ec06a74b0c7e5e4fd370ae13bd0201
Dennis Kievits <denkievits@gmail.com>
2018-10-03 23:28:29 +0200

    Added dropshadow to album detail image

--- frontend/src/components/views/album/detail/DetailStyle.ts
+++ frontend/src/components/views/album/detail/DetailStyle.ts
@@ -43,7 +43,8 @@ const styles: StyleRulesCallback = theme => ({
   },
   image: {
     width: 250,
-    height: 250
+    height: 250,
+    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
   }
 });
 
