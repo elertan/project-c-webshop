c8d4a1f91e37f59987fc5b5a261ef1f0cb9a7985
cbr600f0 <cbr600f0@gmail.com>
2018-10-07 15:12:22 +0200

    trying stuff in the trending page

--- frontend/src/components/views/home/trending/Trending.tsx
+++ frontend/src/components/views/home/trending/Trending.tsx
@@ -1,14 +1,27 @@
 import * as React from 'react';
 import HomeLayout from "../../layout/HomeLayout/HomeLayout";
 import {Typography} from "@material-ui/core";
+import gql from "graphql-tag";
 
 interface IProps {}
 
 class Trending extends React.Component<IProps> {
   public render() {
+
+    const query = gql`
+    {
+      albums {
+        name
+        imageUrl
+      }
+    }
+  `;
+
     return (
       <HomeLayout>
-        <Typography>Trending!</Typography>
+        <Typography>{array.forEach(element => {
+          
+        });</Typography>
       </HomeLayout>
     );
   }
