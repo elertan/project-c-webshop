4daf7f436c3fdb3bd76db81690405eb06f215da0
Dennis Kievits <denkievits@gmail.com>
2018-10-12 03:20:42 +0200

    Implemented basic GraphQL EF

--- backend/Schemas/Types/ArtistType.cs
+++ backend/Schemas/Types/ArtistType.cs
@@ -1,12 +1,13 @@
 using System.Linq;
 using backend_datamodel.Models;
+using GraphQL.EntityFramework;
 using GraphQL.Types;
 
 namespace backend.Schemas.Types
 {
     public class ArtistType : BaseGraphType<Artist>
     {
-        public ArtistType(DatabaseContext db)
+        public ArtistType(DatabaseContext db, IEfGraphQLService efGraphQlService) : base(efGraphQlService)
         {
             Name = "Artist";
 
@@ -15,11 +16,11 @@ namespace backend.Schemas.Types
             Field(a => a.ImageUrl, nullable: true).Description("An image that represents the artist");
             Field(a => a.SpotifyId).Description("The Id that is used on Spotify's database");
 
-            Field<ListGraphType<TrackType>>(
+            AddNavigationField<TrackType, Track>(
                 "tracks",
-                resolve: ctx => db.ArtistXTracks.Where(e => e.ArtistId == ctx.Source.Id).Select(e => e.Track)
-            );
-            Field<ListGraphType<AlbumType>>(
+                resolve: ctx => db.ArtistXTracks.Where(e => e.ArtistId == ctx.Source.Id).Select(e => e.Track));
+
+            AddNavigationField<AlbumType, Album>(
                 "albums",
                 resolve: ctx => db.ArtistXTracks.Where(e => e.ArtistId == ctx.Source.Id)
                     .Join(db.AlbumXTracks,
