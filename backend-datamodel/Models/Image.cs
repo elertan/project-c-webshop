c336da882f1af6638cb4a28ae614d389ddb2e59a
Dennis Kievits <denkievits@gmail.com>
2018-11-02 00:47:20 +0100

    Added optional width and height properties to image model

--- backend-datamodel/Models/Image.cs
+++ backend-datamodel/Models/Image.cs
@@ -3,5 +3,7 @@ namespace backend_datamodel.Models
     public class Image : BaseEntity
     {
         public string Url { get; set; }
+        public int? Width { get; set; }
+        public int? Height { get; set; }
     }
 }
