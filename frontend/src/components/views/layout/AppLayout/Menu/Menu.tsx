5cc92ffa2a1e8a4e238fc26eed88532d0a548e9c
Tim <tim-prins@live.nl>
2018-11-14 11:48:17 +0100

    Added login/register popup

--- frontend/src/components/views/layout/AppLayout/Menu/Menu.tsx
+++ frontend/src/components/views/layout/AppLayout/Menu/Menu.tsx
@@ -1,65 +1,87 @@
-import * as React from 'react';
+import * as React from "react";
 import {
-  Container, Icon, Label,
-  Menu as SemanticMenu, Popup, Search
-} from 'semantic-ui-react';
-import {Subscribe} from "unstated";
+  Container,
+  Icon,
+  Label,
+  Input,
+  Button,Divider,
+  Menu as SemanticMenu,
+  Popup,
+  Search
+} from "semantic-ui-react";
+import { Subscribe } from "unstated";
 import CartState from "../../../../../states/CartState";
 import IProduct from "../../../../../models/IProduct";
 
 // import {NavLink} from "react-router-dom";
 
-interface IProps {
-}
+interface IProps {}
 
-interface IState {
-}
+interface IState {}
 
 class Menu extends React.Component<IProps, IState> {
   public state = {};
 
   public render() {
     return (
-      <div style={{marginBottom: 65}}>
-        <SemanticMenu
-          fixed="top"
-        >
+      <div style={{ marginBottom: 65 }}>
+        <SemanticMenu fixed="top">
           <Container>
-            <SemanticMenu.Item
-              as="a"
-              header
-              href="/"
-            >
+            <SemanticMenu.Item as="a" header href="/">
               Marshmallow's Webshop
             </SemanticMenu.Item>
             <SemanticMenu.Item header>
-              <Search fluid/>
+              <Search fluid />
             </SemanticMenu.Item>
             {/*<NavLink to={"/auth/login"}>*/}
-            <SemanticMenu.Item header position="right" as="a">
-              Log In
-            </SemanticMenu.Item>
+            {/* <SemanticMenu.Item header position="right" as="a"> */}
+
+            <Popup
+              basic
+              hideOnScroll
+              on="click"
+              trigger={
+                <SemanticMenu.Item header position="right" as="a">
+                  <Icon name="sign-in" />
+                  Log In
+                </SemanticMenu.Item>
+              }
+              content={
+                <div>
+                  <h4>E-mail</h4>
+                  <Input transparent inline size="large" placeholder="voorbeeld@voorbeeld.nl" />
+                  <Divider />
+                  <h4>Wachtwoord</h4>
+                  <Input transparent inline size="large"  type="password" placeholder="Wachtwoord" />
+                  <Divider />
+                  <Button primary>Login</Button>
+                  <Button secondary>Registreer</Button>
+                </div>
+              }
+            />
+            {/* </SemanticMenu.Item> */}
             {/*</NavLink>*/}
             <SemanticMenu.Item header as="a">
               Wishlist
             </SemanticMenu.Item>
+
             <Subscribe to={[CartState]}>
               {(cartState: CartState) => (
                 <Popup
                   basic
                   hideOnScroll
-                  on='click'
+                  on="click"
                   trigger={
                     <SemanticMenu.Item header as="a">
                       <Label.Group circular>
-                        <Icon name="cart"/>
+                        <Icon name="cart" />
                         <Label>{cartState.state.products.length}</Label>
                       </Label.Group>
                     </SemanticMenu.Item>
                   }
                   content={
                     <ul>
-                      { cartState.state.products.map((product: IProduct, i) => (
+                      {cartState.state.products.map((product: IProduct, i) => (
                         <li key={i}>{product.album!.name}</li>
                       ))}
                     </ul>
@@ -72,6 +94,6 @@ class Menu extends React.Component<IProps, IState> {
       </div>
     );
   }
-};
+}
 
 export default Menu;
