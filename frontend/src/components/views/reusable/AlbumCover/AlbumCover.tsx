a0d8ae2e685dd2d9f6aa4f85866fe4f81b43c7cb
Joris van der Sluis <jjori8@hotmail.com>
2018-11-14 14:17:27 +0100

    wishlist

--- frontend/src/components/views/reusable/AlbumCover/AlbumCover.tsx
+++ frontend/src/components/views/reusable/AlbumCover/AlbumCover.tsx
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom';
 import IImage from "../../../../models/IImage";
 import ProgressiveImage from "../ProgressiveImage/ProgressiveImage";
 import {Typography} from "@material-ui/core";
+import { Button } from 'semantic-ui-react';
+import { Subscribe } from 'unstated';
+import WishState from 'src/states/WishState';
+import IProduct from "src/models/IProduct";
 
 interface IProps {
   name: string;
@@ -11,8 +15,11 @@ interface IProps {
   id: number;
 }
 
+
+
 const AlbumCover: React.SFC<IProps> = (props: IProps) => {
   return (
+    <div>
     <Link to={`/album/${props.id}`}>
       <div
         className="AlbumCover-root"
@@ -30,11 +37,39 @@ const AlbumCover: React.SFC<IProps> = (props: IProps) => {
             className="AlbumCover-img"
             src="default?"
           />
-        }
+          
+        } 
+       
+        
+        
         <Typography className="AlbumCover-name">{props.name}</Typography>
       </div>
     </Link>
+     <Subscribe to={[WishState]}>
+     {wishState => (
+       <Button icon = "heart" onClick={addToWish(props,wishState)}/>
+     )}
+     </Subscribe>
+     </div>
   );
+ 
+};
+const addToWish = (props: IProps ,wishState: WishState) => () => {
+  const product: IProduct = {
+    id: wishState.state.products.length + 1,
+    album: {
+      id: props.id,
+      name: props.name,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  };
+  
+  if(wishState.state.products.filter(el => props.id === el)){
+    console.log('error');
+  }
+  else{
+  wishState.setState({ products: [...wishState.state.products, product] });}
 };
 
 export default AlbumCover;
