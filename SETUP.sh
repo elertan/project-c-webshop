9fee0580f8d21731749db1d68945056b00b688bb
Dennis Kievits <denkievits@gmail.com>
2018-11-14 20:54:12 +0100

    Unix script!

---
+++ SETUP.sh
@@ -0,0 +1,18 @@
+
+echo "This script will set up the project assuming all required software/tools is installed."
+echo "If this is *NOT* your first time running this, please remove all tables/the entire database project-c-webshop from Postgres."
+echo "MAKE SURE THE PREVIOUS STATEMENT IS COMPLETED."
+read -p "Press any key to continue..."
+
+echo "Building data model:"
+cd ./backend-datamodel
+dotnet build
+
+echo "Updating database with latest structure"
+dotnet ef database update
+
+echo "Run filling tool"
+cd ../database-filling-tool
+dotnet run
+
+echo "If nothing failed, assume everything worked"
