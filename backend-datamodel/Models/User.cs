065cae527cba38eda80d9f1189911140f8ee14e5
Dennis Kievits <denkievits@gmail.com>
2018-11-15 16:01:34 +0100

    Updated User Fields

--- backend-datamodel/Models/User.cs
+++ backend-datamodel/Models/User.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace backend_datamodel.Models
 {
     public class User : BaseEntity
@@ -7,6 +9,7 @@ namespace backend_datamodel.Models
         public string Password { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        public DateTime? DateOfBirth { get; set; }
         public string Token { get; set; }
     }
 }
