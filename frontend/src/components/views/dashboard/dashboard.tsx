f7c2f8e86afb12920698e2e157f702867bccb5a4
Tim <tim-prins@live.nl>
2018-11-20 22:17:52 +0100

    Added the buttons for the dashboard

--- frontend/src/components/views/dashboard/dashboard.tsx
+++ frontend/src/components/views/dashboard/dashboard.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import AppLayout from "../layout/AppLayout/AppLayout";
-import { Menu, Divider } from "semantic-ui-react";
+import { Menu, Divider, Table, Form, Grid, Button } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
 export default class Dashboard extends React.Component {
@@ -33,6 +33,83 @@ export default class Dashboard extends React.Component {
             </NavLink>
           </Menu.Item>
         </Menu>
+
+        <Table>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell width={5}>
+                <h3>
+                  <b>Email : </b>
+                </h3>
+                {"tim-prins@live.nl"}
+              </Table.Cell>
+              <Table.Cell>
+                <Button animated size="large" fluid>
+                  <Button.Content visible>Edit</Button.Content>
+                  <Button.Content hidden>Verander e-mail</Button.Content>
+                </Button>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell width={5}>
+                <h3>
+                  <b>Naam : </b>
+                </h3>
+                {"Tim Prins"}
+              </Table.Cell>
+              <Table.Cell>
+                <Button animated size="large" fluid>
+                  <Button.Content visible>Edit</Button.Content>
+                  <Button.Content hidden>Verander naam</Button.Content>
+                </Button>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell width={5}>
+                <h3>
+                  <b>Wachtwoord : </b>
+                </h3>
+                {"*******"}
+              </Table.Cell>
+              <Table.Cell>
+                <Button animated size="large" fluid>
+                  <Button.Content visible>Edit</Button.Content>
+                  <Button.Content hidden>Nieuw wachtwoord</Button.Content>
+                </Button>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+
+        <Form>
+          <Grid columns="two">
+            <Grid.Row>
+              <Grid.Column>
+                <Form.Field
+                  label={"Huidige naam hoort hier"}
+                  control="input"
+                  placeholder="Naam"
+                />
+              </Grid.Column>
+              <Grid.Column>
+                <Form.Field
+                  label={"Huidige achternaam hoort hier"}
+                  control="input"
+                  placeholder="Achternaam"
+                />
+              </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row>
+              <Grid.Column>
+                <h1>test</h1>
+              </Grid.Column>
+              <Grid.Column>
+                <h1>test</h1>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Form>
       </AppLayout>
     );
   }
