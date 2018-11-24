7e718f75d7df79da468fb30a03f4b91721a408ed
Robrecht Cornelis <robrechtcornelis7@hotmail.com>
2018-11-24 14:48:26 +0100

    Progress Category Page (dummy data)

--- frontend/src/components/views/category/detail/CategoryDetail.tsx
+++ frontend/src/components/views/category/detail/CategoryDetail.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import AppLayout from "../../layout/AppLayout/AppLayout";
-// import AlbumCover from "../../reusable/AlbumCover/AlbumCover";
-// import ArtistCover from "../../reusable/ArtistCover/ArtistCover";
 import {Grid} from "semantic-ui-react";
 import gql from "graphql-tag";
 import {Query} from "react-apollo";
 import CategoryCover from '../../reusable/CategoryCover/CategoryCover';
-import artistPlaceholder from '../../../../img/artist.jpg';
-import albumPlaceholder from '../../../../img/album.jpg';
+import AlbumCover from '../../reusable/AlbumCover/AlbumCover';
+import ArtistCover from "../../reusable/ArtistCover/ArtistCover";
 
 interface IProps {
     categoryId: number;
@@ -19,31 +17,105 @@ class CategoryDetail extends React.Component<IProps> {
         {
             categories(ids: "${this.props.categoryId}") {
                 items {
-                    id
                     name
+                    id
+                    images(orderBy: {path: "height", descending: true}, first: 1) {
+                        items {
+                          url
+                        }
+                      }
                 }
             }
         }`;
 
+        const dummyAlbumsQuery = gql`
+        {
+            albums(ids:["7", "5", "3", "12", "18", "16", "30", "28", "19", "31"]) {
+                items {
+                  name
+                  id
+                  images(orderBy: {path: "height", descending: true}, first: 1) {
+                    items {
+                      url
+                    }
+                  }
+                }
+              }
+        }`;
+
+        const dummyArtistsQuery = gql`
+        {
+            artists(ids:["1", "2", "3", "25", "18", "17", "30", "40", "9", "69"]) {
+                items {
+                  name
+                  id
+                  images(orderBy: {path: "height", descending: true}, first: 1) {
+                    items {
+                      url
+                    }
+                  }
+                }
+              }
+        }`;
+
+      return (
+        <AppLayout>
+          <h1>Category</h1>
+          <Query query={query}>
+            {({ loading, error, data }) => {
+              if (loading) {
+                return null;
+              }
+              if (error) {
+                return <span>{error.message}</span>
+              }
+              return <div>{this.renderDetail(data.categories.items)}</div>
+            }}
+          </Query>
+          <h1>Albums</h1>
+          <Query query={dummyAlbumsQuery}>
+            {({ loading, error, data }) => {
+              if (loading) {
+                return null;
+              }
+              if (error) {
+                return <span>{error.message}</span>
+              }
+              return <div>{this.renderAlbumsDetail(data.albums.items)}</div>
+            }}
+          </Query>
+          <h1>Artists</h1>
+
+          <Query query={dummyArtistsQuery}>
+            {({ loading, error, data }) => {
+              if (loading) {
+                return null;
+              }
+              if (error) {
+                return <span>{error.message}</span>
+              }
+              return <div>{this.renderArtistsDetail(data.artists.items)}</div>
+            }}
+          </Query>
+        </AppLayout>
+      )
+    }
+
+    private renderDetail = (categories: any[]) => {
         return (
-            <AppLayout>
-              <Query query={query}>
-                {({loading, error, data }) => {
-                  if (loading) {
-                    return null;
-                  }
-                  if (error) {
-                    return <span>{error.message}</span>
-                  }
-                  console.log(data);
-                  return <div>{this.renderDetail(data.categories.items)}</div>
-                }}
-              </Query>
-            </AppLayout>
+          <div>
+            {categories.map((category: any, i: number) => 
+                <CategoryCover
+                    key={i}
+                    id={category.id}
+                    name={category.name}
+                    imageSource={category.images.items} />
+                )}     
+          </div>
         )
     }
 
-    private renderDetail = (categories:any[]) => {
+    private renderAlbumsDetail = (albums:any[]) => {
         return (
           <div>
             <div style={{ marginTop: 15 }}>
@@ -51,26 +123,33 @@ class CategoryDetail extends React.Component<IProps> {
                 columns={4}
                 doubling
                 >
-                {categories.map((category: any, i: number) =>
-                <CategoryCover // Album Cover
-                    key={i} 
-                    id={category.id} 
-                    name="Album name" 
-                    imageSource={albumPlaceholder}/>
-                )}
+                    {albums.map((album: any, i: number) =>
+                    <AlbumCover
+                        key={i} 
+                        id={album.id} 
+                        name={album.name} 
+                        imageSource={album.images.items}/>
+                    )}
                 </Grid>
             </div>
+          </div>
+        )
+    }
+
+    private renderArtistsDetail = (artists:any[]) => {
+        return (
+          <div>
             <div style={{ marginTop: 15 }}>
                 <Grid
                 columns={4}
                 doubling
                 >
-                {categories.map((category: any, i: number) =>
-                <CategoryCover // Artist Cover
-                    key={i} 
-                    id={category.id} 
-                    name="Artist name"
-                    imageSource={artistPlaceholder}/>
+                  {artists.map((artist: any, i: number) =>
+                  <ArtistCover
+                      key={i} 
+                      id={artist.id} 
+                      name={artist.name}
+                      imageSource={artist.images.items}/>
                 )}
                 </Grid>
             </div>
