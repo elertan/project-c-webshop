e1bd44246b7e31895950dcb9a0c1803535706bfd
Tim <tim-prins@live.nl>
2018-11-27 18:01:07 +0100

    Added the reset password page and partially added the hide password functionality

--- frontend/src/components/views/dashboard/accountDetails/passwordReset/passwordReset.tsx
+++ frontend/src/components/views/dashboard/accountDetails/passwordReset/passwordReset.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import AppLayout from "../../../layout/AppLayout/AppLayout";
-import { Table, Form } from "semantic-ui-react";
+import { Table, Form, Input, Button } from "semantic-ui-react";
 import DashboardMenu from "../../../reusable/DashboardMenu/DashboarMenu";
 
 const styles = {
@@ -12,7 +12,16 @@ const styles = {
 };
 
 class PasswordReset extends React.Component {
-  public state = {};
+  public state = {
+    ButtonText: "Show all passwords",
+    PasswordInput: "password"
+  };
+
+  public toggleOldPasswordVisibility = () =>
+    this.setState({
+      ButtonText: "Hide all passwords",
+      PasswordInput: ""
+    });
 
   public render() {
     return (
@@ -23,8 +32,51 @@ class PasswordReset extends React.Component {
             <Table>
               <Table.Body>
                 <Table.Row>
-                  <Table.Cell />
-                  <Table.Cell />
+                  <Table.Cell>
+                    <h3>Current password :</h3>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Input
+                      placeholder="Current password"
+                      type={this.state.PasswordInput}
+                    />
+                  </Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.Cell>
+                    <h3>New password :</h3>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Input
+                      placeholder="New password"
+                      type={this.state.PasswordInput}
+                    />
+                  </Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.Cell>
+                    <h3>Confirm new password :</h3>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Input
+                      placeholder="New password"
+                      type={this.state.PasswordInput}
+                    />
+                  </Table.Cell>
+                </Table.Row>
+
+                <Table.Row textAlign="center">
+                  <Table.Cell>
+                    <Button color="green">Done</Button>
+                    <Button
+                      color="blue"
+                      onClick={this.toggleOldPasswordVisibility}
+                    >
+                      {this.state.ButtonText}
+                    </Button>
+                  </Table.Cell>
                 </Table.Row>
               </Table.Body>
             </Table>
