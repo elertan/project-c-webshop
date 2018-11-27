7cc0b6e95e8792040c85fa1f829d64b0466edb49
Tim <tim-prins@live.nl>
2018-11-27 15:24:41 +0100

    Added the overview in the routes

---
+++ frontend/src/components/views/dashboard/overview/overview.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import AppLayout from "../../layout/AppLayout/AppLayout";
+import { } from "semantic-ui-react";
+import DashboardMenu from "../../reusable/DashboardMenu/DashboarMenu";
+
+class Overview extends React.Component {
+  public state = {};
+
+  public render() {
+    return (
+      <AppLayout>
+        <DashboardMenu />
+        <h1> Overview </h1>
+      </AppLayout>
+    );
+  }
+}
+
+export default Overview;
--- frontend/src/routes/DashboardRouter.tsx
+++ frontend/src/routes/DashboardRouter.tsx
@@ -3,6 +3,7 @@ import {Route, RouteComponentProps, Switch} from "react-router";
 import AcountDetailsContainer from "../components/containers/dashboard/accountDetails/accountDetailsContainer";
 import PaymentMethodsContainer from "../components/containers/dashboard/paymentDetails/paymentDetailsContainer"
 import OrderHistoryContainer from "../components/containers/dashboard/orderHistory/orderHistoryContainer";
+import OverviewContainer from "../components/containers/dashboard/overview/overviewContainer";
 import NotFound from "../components/views/errors/NotFound/NotFound";
 
 interface IProps extends RouteComponentProps<{}> {}
@@ -13,6 +14,7 @@ const DashboardRouter: React.SFC<IProps> = (props: IProps) => {
       <Route exact path={`${props.match.url}/accountdetails`} component={AcountDetailsContainer} />
       <Route exact path={`${props.match.url}/orderhistory`} component={OrderHistoryContainer} />
       <Route exact path={`${props.match.url}/paymentmethods`} component={PaymentMethodsContainer} />
+      <Route exact path={`${props.match.url}/overview`} component={OverviewContainer} />
 
       <Route component={NotFound} />
     </Switch>
