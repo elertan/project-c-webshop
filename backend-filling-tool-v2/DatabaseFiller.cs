578bd0a38a996db76f7f34eb7a73866462ffdb83
Dennis Kievits <denkievits@gmail.com>
2018-11-28 13:56:16 +0100

    Duplicate artist fix

--- backend-filling-tool-v2/DatabaseFiller.cs
+++ backend-filling-tool-v2/DatabaseFiller.cs
@@ -136,10 +136,10 @@ namespace backend_filling_tool_v2
 
                 var artists = dataset.FullArtists.Select(ExtractFullArtist);
                 
-                _logger.Log("Created artist entities");
-                await db.Artists.AddRangeAsync(artists);
-                await db.SaveChangesAsync();
-                _logger.Log("Saved artist entities to database");
+//                _logger.Log("Created artist entities");
+//                await db.Artists.AddRangeAsync(artists);
+//                await db.SaveChangesAsync();
+//                _logger.Log("Saved artist entities to database");
                 _logger.Log("Creating many-to-many relationships between the tables");
                 
                 _logger.Log("Creating Album X Track relationships...");
