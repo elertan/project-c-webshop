6dee0da496aae7c3a88c4dd474c86340176e1c96
Dennis Kievits <denkievits@gmail.com>
2018-12-06 13:27:26 +0100

    Graph fixes

--- backend/Schemas/RootMutation.cs
+++ backend/Schemas/RootMutation.cs
@@ -13,12 +13,71 @@ namespace backend.Schemas
 {
     public class RootMutation : EfObjectGraphType<object>
     {
-        public RootMutation(IEfGraphQLService service) : base(service)
+        private readonly IAccountService _accountService;
+        private readonly IOrderService _orderService;
+
+        public RootMutation(IEfGraphQLService service, IAccountService accountService, IOrderService orderService) : base(service)
         {
+            _accountService = accountService;
+            _orderService = orderService;
             Name = "Mutation";
 
-            Field<AuthGraph>("auth");
-            Field<Graphs.Mutations.Order.MutationOrderGraph>("order");
+            Field<ApiResultGraph<UserGraph, User>>(
+                "register",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<RegisterInput>> {Name = "data"}
+                ),
+                resolve: CreateAccountResolveFn);
+
+            Field<ApiResultGraph<UserGraph, User>>(
+                "login",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<LoginInput>> {Name = "data"}),
+                resolve: LoginResolveFn);
+            
+            Field<ApiResultGraph<OrderGraph, Order>>(
+                "createAnonymousOrder",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<CreateAnonymousOrderInput>> {Name = "data"}
+                ),
+                resolve: CreateAnonymousOrderResolveFn
+            );
+        }
+        
+        private async Task<ApiResult<User>> CreateAccountResolveFn(ResolveFieldContext<object> context)
+        {
+            var data = context.GetArgument<RegisterData>("data");
+            try
+            {
+                var user = await _accountService.Register(data);
+                return new ApiResult<User> { Data = user };
+            }
+            catch (Exception ex)
+            {
+                return new ApiResult<User> { Errors = new List<ApiError> {new ApiError {Message = ex.Message}} };
+            }
+        }
+
+        private async Task<ApiResult<User>> LoginResolveFn(ResolveFieldContext<object> context)
+        {
+            var data = context.GetArgument<LoginData>("data");
+            try
+            {
+                var user = await _accountService.Login(data);
+                return new ApiResult<User> {Data = user};
+            }
+            catch (Exception ex)
+            {
+                return new ApiResult<User> { Errors = new List<ApiError> {new ApiError {Message = ex.Message}} };
+            }
+        }
+        
+        private async Task<ApiResult<Order>> CreateAnonymousOrderResolveFn(ResolveFieldContext<object> context)
+        {
+            var data = context.GetArgument<CreateAnonymousOrderData>("data");
+            
+            var order = await _orderService.CreateAnonymousOrder(data);
+            return new ApiResult<Order>{Data = order};
         }
     }
 }
