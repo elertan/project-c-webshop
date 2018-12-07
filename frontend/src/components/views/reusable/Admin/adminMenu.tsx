eb2f2b8aaef986b12d8d43f7220b7e1c7b027338
Tim <tim-prins@live.nl>
2018-12-07 14:47:20 +0100

    Added menu in the admin-dashboard

--- frontend/src/components/views/reusable/Admin/adminMenu.tsx
+++ frontend/src/components/views/reusable/Admin/adminMenu.tsx
@@ -1,50 +1,44 @@
 import * as React from "react";
-import { Segment, Menu } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
+import { Menu, Segment } from "semantic-ui-react";
+interface IProps {}
 const styles = {
   size: {
     width: "15vw",
     margin: "1vw"
   }
 };
-
-class Admin extends React.Component {
-  public state = { activeItem: "overview" };
+class AdminMenu extends React.Component<IProps> {
+  public state = { activeItem: "home" };
 
   public changeMenuItem(name: string) {
     this.setState({
       activeItem: name
     });
   }
-
   public render() {
     const { activeItem } = this.state;
-
     return (
       <div style={styles.size}>
         <Segment inverted>
           <Menu inverted pointing vertical secondary fluid>
-            <NavLink to={"/admin/admin"}>
+            <NavLink to={"/dashboard/overview"}>
               <Menu.Item
                 name="Overview"
                 active={activeItem === "Overview"}
                 onClick={() => this.changeMenuItem("Overview")}
               />
             </NavLink>
-            <NavLink to={"/admin/admin"}>
-              <Menu.Item
-                name="Users"
-                active={activeItem === "Users"}
-                onClick={() => this.changeMenuItem("Users")}
-              />
-            </NavLink>
-            <NavLink to={"/admin/admin"}>
-              <Menu.Item
-                name="Statistics"
-                active={activeItem === "Statistics"}
-                onClick={() => this.changeMenuItem("Statistics")}
-              />
-            </NavLink>
+            <Menu.Item
+              name="Users"
+              active={activeItem === "Users"}
+              onClick={() => this.changeMenuItem("Users")}
+            />
+            <Menu.Item
+              name="Statistics"
+              active={activeItem === "Statistics"}
+              onClick={() => this.changeMenuItem("Statistics")}
+            />
           </Menu>
         </Segment>
       </div>
@@ -52,4 +46,4 @@ class Admin extends React.Component {
   }
 }
 
-export default Admin;
+export default AdminMenu;
