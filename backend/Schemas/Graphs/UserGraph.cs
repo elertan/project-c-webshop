1ce3defb8b7caa1848db6ccb17b4f332713cfed9
Robrecht Cornelis <robrechtcornelis7@hotmail.com>
2018-12-12 20:59:02 +0100

    User graph now has DoB queriable field

--- backend/Schemas/Graphs/UserGraph.cs
+++ backend/Schemas/Graphs/UserGraph.cs
@@ -10,6 +10,7 @@ namespace backend.Schemas.Graphs
             Field(u => u.Email).Description("The email of the user");
             Field(u => u.Firstname).Description("The firstname of the user");
             Field(u => u.Lastname).Description("The lastname of the user");
+            Field(u => u.DateOfBirth).Description("The birthdate of the user");
             Field(u => u.Password, nullable: true).Description("The hashed password of the user");
             Field(u => u.Token, nullable: true).Description("JWT Token");
             Field(u => u.AnonymousRegistrationToken, nullable: true)
