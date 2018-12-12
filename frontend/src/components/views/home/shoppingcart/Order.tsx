5ec60088c0334087f47b876772dd883201bef544
Dennis Kievits <denkievits@gmail.com>
2018-12-12 14:03:36 +0100

    Confirm order bank selection

--- frontend/src/components/views/home/shoppingcart/Order.tsx
+++ frontend/src/components/views/home/shoppingcart/Order.tsx
@@ -11,19 +11,17 @@ import {
   Label,
   Button,
 } from "semantic-ui-react";
-import { Subscribe } from "unstated";
+import {Subscribe} from "unstated";
 import CartState from "src/states/CartState";
 import IProduct from "src/models/IProduct";
-import {  Redirect } from "react-router-dom";
-import { Field, FieldProps, Formik, FormikProps } from "formik";
-import { withApollo, WithApolloClient } from "react-apollo";
+import {Redirect} from "react-router-dom";
+import {Field, FieldProps, Formik, FormikProps} from "formik";
+import {withApollo, WithApolloClient} from "react-apollo";
 import OrderState from "src/states/OrderState";
 import gql from "graphql-tag";
 import IApiResult from "src/models/IApiResult";
 import IUser from "src/models/IUser";
 import IApiError from "src/models/IApiError";
-
-
 
 const createAnonymousOrderMutation = gql`
   mutation($data: CreateAnonymousOrderInput!) {
@@ -41,7 +39,9 @@ const createAnonymousOrderMutation = gql`
   }
 `;
 
-interface IProps {}
+interface IProps {
+}
+
 const styles = {
   BankPositioning: {
     margin: "1.5%"
@@ -68,15 +68,17 @@ const styles = {
 interface IFormikValues {
   email: string;
   status: string;
+  bank: number | null;
 }
 
 const initialValues: IFormikValues = {
   email: "",
-  status: "email"
+  status: "email",
+  bank: null
 };
 
 const bankOptions = [
-  "ING", 
+  "ING",
   "ABN Amro",
   "Rabobank"
 ];
@@ -95,10 +97,10 @@ class Order extends React.Component<WithApolloClient<IProps>> {
     value: "",
 
   };
- 
- 
+
+
   public render() {
-    
+
     return (
       <Subscribe to={[OrderState, CartState]}>{this.orderRender}</Subscribe>
     );
@@ -109,7 +111,7 @@ class Order extends React.Component<WithApolloClient<IProps>> {
       <AppLayout>
         <div style={styles.HeaderPositioning}>
           <Header as="h2">
-            <Icon name="payment" />
+            <Icon name="payment"/>
             <Header.Content>
               Your order
               <Header.Subheader>
@@ -157,7 +159,7 @@ class Order extends React.Component<WithApolloClient<IProps>> {
                   active={this.state.status === "email"}
                   disabled={this.state.status !== "email"}
                 >
-                  <Icon name="truck" />
+                  <Icon name="truck"/>
                   <Step.Content>
                     <Step.Title>Shipping</Step.Title>
                   </Step.Content>
@@ -166,7 +168,7 @@ class Order extends React.Component<WithApolloClient<IProps>> {
                   active={this.state.status === "bank"}
                   disabled={this.state.status !== "bank"}
                 >
-                  <Icon name="credit card" />
+                  <Icon name="credit card"/>
                   <Step.Content>
                     <Step.Title>Billing</Step.Title>
                   </Step.Content>
@@ -175,7 +177,7 @@ class Order extends React.Component<WithApolloClient<IProps>> {
                   active={this.state.status === "confirm"}
                   disabled={this.state.status !== "confirm"}
                 >
-                  <Icon name="info" />
+                  <Icon name="info"/>
                   <Step.Content>
                     <Step.Title>Confirm Order</Step.Title>
                   </Step.Content>
@@ -211,23 +213,23 @@ class Order extends React.Component<WithApolloClient<IProps>> {
         <Table.HeaderCell colSpan="2">
           <h3>Shipping details</h3>
         </Table.HeaderCell>
-        <Table.Body >
+        <Table.Body>
           <Table.Row>
             <Table.Cell>Emailaddress</Table.Cell>
-            <Table.Cell />
+            <Table.Cell/>
             <Table.Cell>
-        <Input
-          id="email"
-          iconPosition="left"
-          placeholder="johndoe@example.com"
-          size="large"
-          error={Boolean(error)}
-        >
-          <Icon name="at" />
-          <input {...fieldProps.field} />
-        </Input>
-        </Table.Cell>
-        </Table.Row>
+              <Input
+                id="email"
+                iconPosition="left"
+                placeholder="johndoe@example.com"
+                size="large"
+                error={Boolean(error)}
+              >
+                <Icon name="at"/>
+                <input {...fieldProps.field} />
+              </Input>
+            </Table.Cell>
+          </Table.Row>
         </Table.Body>
         <Button
           floated="right"
@@ -245,51 +247,48 @@ class Order extends React.Component<WithApolloClient<IProps>> {
       </Form.Field>
     );
   };
-  
-   
 
-   
 
   private renderBankField = (fieldProps: FieldProps<IFormikValues>) => {
-   
-  
     return (
       <Form.Field>
         <Table.HeaderCell colSpan="2">
           <h3>Billing details</h3>
         </Table.HeaderCell>
-        <Table.Body >
+        <Table.Body>
           <Table.Row>
             <Table.Cell>Bank:</Table.Cell>
-            <Table.Cell />
+            <Table.Cell/>
             <Table.Cell>
-             <select>
-              <option value={String(null)} disabled>Choose your bank</option>
-              {bankOptions.map((bank, i)=> (
-                <option key={i} value={i}>{bank}</option>
-              ))}
-             </select> 
-           
-       </Table.Cell>
-       <p>{this.state.value}</p>
-        </Table.Row>
-        </Table.Body >
-        <Button positive floated="right" onClick={() => this.setStatus("confirm")}>Next</Button>
+              <select
+                value={String(fieldProps.field.value)}
+                onChange={ev => fieldProps.form.setFieldValue(fieldProps.field.name, Number(ev.target.value))}
+              >
+                <option value={String(null)} disabled>Choose your bank</option>
+                {bankOptions.map((bank, i) => (
+                  <option key={i} value={String(i)}>{bank}</option>
+                ))}
+              </select>
+            </Table.Cell>
+            <p>{this.state.value}</p>
+          </Table.Row>
+        </Table.Body>
+        <Button positive floated="right" onClick={() => this.setStatus("confirm")} disabled={fieldProps.field.value === null}>Next</Button>
       </Form.Field>
     );
   };
 
-  
+
   private renderConfirmationField = (fieldProps: FieldProps<IFormikValues>) => {
     return (
       <Form.Field>
         <p>your email is: {fieldProps.form.values.email}</p>
-        <p>your bank is: </p>
-          <Button
-            onClick={() => this.handleOrder(fieldProps.form.values.email)}
-          >
-            Send
-          </Button>
+        <p>your bank is: {bankOptions[fieldProps.form.values.bank!]}</p>
+        <Button
+          onClick={() => this.handleOrder(fieldProps.form.values.email)}
+        >
+          Send
+        </Button>
       </Form.Field>
     );
   };
@@ -321,11 +320,11 @@ class Order extends React.Component<WithApolloClient<IProps>> {
     if (apiResult.errors) {
       this.setState({errors: apiResult.errors})
       console.log(apiResult.errors)
-     
+
     } else {
       if (this.state.errors.length > 0) {
         this.setState({errors: []})
-        
+
         return <Redirect to="/confirmorder"/>
       }
       return <Redirect to="./confirmorder"/>
@@ -333,35 +332,33 @@ class Order extends React.Component<WithApolloClient<IProps>> {
     return <Redirect to="./confirmorder"/>
   };
 
-    
-  
 
   private setStatus = (newStatus: string) => {
-    this.setState({ status: newStatus });
+    this.setState({status: newStatus});
   };
 
   private renderFormik = (formik: FormikProps<IFormikValues>) => {
     if (this.state.status === "email") {
       return (
         <Form className="ui form">
-          <Field name="email" render={this.renderEmailField} />
+          <Field name="email" render={this.renderEmailField}/>
         </Form>
       );
     }
     if (this.state.status === "bank") {
       return (
         <Form className="ui form">
-          <Field name="bank" render={this.renderBankField} />
+          <Field name="bank" render={this.renderBankField}/>
         </Form>
       );
     }
     return (
       <Form className="ui form">
-        <Field name="confirm" render={this.renderConfirmationField} />
+        <Field name="confirm" render={this.renderConfirmationField}/>
         {this.state.errors.length > 0 &&
-          <p
-            style={{textAlign: 'center', marginTop: 15, color: 'red'}}>{(this.state.errors[0] as IApiError).message}</p>
-          }
+        <p
+          style={{textAlign: 'center', marginTop: 15, color: 'red'}}>{(this.state.errors[0] as IApiError).message}</p>
+        }
       </Form>
     );
   };
