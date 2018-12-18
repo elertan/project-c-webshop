356f350546230bb5948cd487478b0289883a0e7a
Joris van der Sluis <jjori8@hotmail.com>
2018-12-19 00:23:07 +0100

    Added redirect when shoppingcart is empty

--- frontend/src/routes/ShoppingcartRouter.tsx
+++ frontend/src/routes/ShoppingcartRouter.tsx
@@ -1,23 +1,40 @@
-import * as React from 'react';
-import {Route, RouteComponentProps, Switch} from "react-router";
+import * as React from "react";
+import { Route, RouteComponentProps, Switch, Redirect } from "react-router";
 
 import NotFound from "../components/views/errors/NotFound/NotFound";
-import ShoppingcartContainer from 'src/components/containers/home/shoppingcart/ShoppingcartContainer';
-import OrderContainer from 'src/components/containers/home/shoppingcart/OrderContainer';
-
-
-
+import ShoppingcartContainer from "src/components/containers/home/shoppingcart/ShoppingcartContainer";
+import OrderContainer from "src/components/containers/home/shoppingcart/OrderContainer";
+import { Subscribe } from "unstated";
+import CartState from "src/states/CartState";
 
 interface IProps extends RouteComponentProps<{}> {}
 
 const ShoppingcartRouter: React.SFC<IProps> = (props: IProps) => {
   return (
     <Switch>
-      <Route exact path={`${props.match.url}`} component={ShoppingcartContainer} />
-      
-      <Route exact path={`${props.match.url}/order`} component={OrderContainer} />
-      
-      <Route component={NotFound} />
+      <Subscribe to={[CartState]}>
+        {(cartState: CartState) => {
+          // Don't make routes visible to guest users
+          if (cartState.state.products.length === 0) {
+            return <Redirect to="/home/explore" />;
+          }
+          return (
+            <>
+              <Route
+                exact
+                path={`${props.match.url}`}
+                component={ShoppingcartContainer}
+              />
+              <Route
+                exact
+                path={`${props.match.url}/order`}
+                component={OrderContainer}
+              />
+              <Route component={NotFound} />
+            </>
+          );
+        }}
+      </Subscribe>
     </Switch>
   );
 };
