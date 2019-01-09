f5f2e9c85c303760fff100844116ad3ca2695d42
Dennis Kievits <denkievits@gmail.com>
2019-01-09 16:15:27 +0100

    Werkt bullshit

--- backend/Services/OrderService.cs
+++ backend/Services/OrderService.cs
@@ -21,12 +21,14 @@ namespace backend.Services
     {
         private readonly DatabaseContext _db;
         private readonly IAccountService _accountService;
-        
+        private readonly IEmailService _emailService;
 
-        public OrderService(DatabaseContext db, IAccountService accountService)
+
+        public OrderService(DatabaseContext db, IAccountService accountService, IEmailService emailService)
         {
             _db = db;
             _accountService = accountService;
+            _emailService = emailService;
         }
 
         /// <summary>
@@ -53,6 +55,33 @@ namespace backend.Services
             await _db.AddAsync(order);
             await _db.AddRangeAsync(orderXProductEntries);
             await _db.SaveChangesAsync();
+            
+            var albums = await _db.Albums.Where(x => data.ProductIds.Contains(x.ProductId)).ToListAsync();
+            var tracks = await _db.Tracks.Where(x => data.ProductIds.Contains(x.ProductId)).ToListAsync();
+            
+            await _emailService.SendEmail(
+                new MailAddress(user.Email),
+                "We've received your order",
+                "Hi there!\n\nWe have successfully received your order.\nPlease sit back, in a few seconds we'll sent you a mail where you can access your purchased content.\n\nThanks for choosing The flying marshmallows"
+            );
+
+            await Task.Delay(3000);
+            
+            await _emailService.SendEmail(
+                new MailAddress(user.Email),
+                "Download your content",
+                "As promised.\n\nYou can either go to the webshop to view your content, or use the links below to access the albums/tracks you purchased.\n\n" +
+                (albums.Count == 0 ?
+                    ""
+                    :
+                    "Albums:\n" + albums.Select(x => "-" + x.Name).Aggregate((curr, next) => curr + "\n" + next))
+                +
+                (tracks.Count == 0 ?
+                    ""
+                    :
+                    "Tracks:\n" + tracks.Select(x => "-" + x.Name).Aggregate((curr, next) => curr + "\n" + next))
+                + "\nEnjoy the music!"
+            );
 
             return order;
         }
@@ -88,10 +117,36 @@ namespace backend.Services
                 Order = order,
                 ProductId = id
             });
+
+            var albums = await _db.Albums.Where(x => data.ProductIds.Contains(x.ProductId)).ToListAsync();
+            var tracks = await _db.Tracks.Where(x => data.ProductIds.Contains(x.ProductId)).ToListAsync();
             
             await _db.AddAsync(order);
             await _db.AddRangeAsync(orderXProductEntries);
-            await _db.SaveChangesAsync();   
+            await _db.SaveChangesAsync();
+            await _emailService.SendEmail(
+                new MailAddress(user.Email),
+                "We've received your order",
+                "Hi there!\n\nWe have successfully received your order.\nPlease sit back, in a few seconds we'll sent you a mail where you can access your purchased content.\n\nThanks for choosing The flying marshmallows"
+            );
+
+            await Task.Delay(3000);
+            
+            await _emailService.SendEmail(
+                new MailAddress(user.Email),
+                "Download your content",
+                "As promised.\n\nYou can either go to the webshop to view your content, or use the links below to access the albums/tracks you purchased.\n\n" +
+                (albums.Count == 0 ?
+                    ""
+                    :
+                    "Albums:\n" + albums.Select(x => "-" + x.Name).Aggregate((curr, next) => curr + "\n" + next))
+                +
+                (tracks.Count == 0 ?
+                    ""
+                    :
+                    "Tracks:\n" + tracks.Select(x => "-" + x.Name).Aggregate((curr, next) => curr + "\n" + next))
+                + "\nEnjoy the music!"
+            );
 
             return order;
         }
